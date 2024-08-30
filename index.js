b7793de9988cd32d579b4e9c688e20699ea9c79a
Manan Gandhi <ardumanan@gmail.com>
2024-08-30 22:43:11 +0530

    Feat: Added handlers for 404 and 5xx errors

--- index.js
+++ index.js
@@ -14,4 +14,23 @@ app.use(
 
 app.use("/auth", authRouter);
 
+app.use(function (req, res, next) {
+  res.status(404).json({
+    success: false,
+    message: "This route could not be found",
+    data: null,
+  });
+});
+
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500).json({
+    success: false,
+    message: "An unexpected error occured",
+    data: null,
+  });
+});
+
 export default app;
