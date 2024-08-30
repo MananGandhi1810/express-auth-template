1a77928dcd96dc7f9f50b11a8eab5867924b00e5
Manan Gandhi <ardumanan@gmail.com>
2024-08-30 22:47:03 +0530

    Feat: Added morgan logger in dev mode

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import express from "express";
 import cors from "cors";
 import authRouter from "./router/auth.js";
+import logger from "morgan";
 
 const app = express();
 
+app.use(logger("dev"));
 app.use(express.json());
 app.use(
   cors({
     origin: true,
     credentials: true,
   }),
-);
+);  
 
 app.use("/auth", authRouter);
 
